1cf67a0e8b2d41d6ce3bd4dd99cb8c4731151022
Dev Lukale <devluka99@gmail.com>
2023-12-25 09:47:10 +0300

    added single course page in teacher mode to complete setting up the course

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,14 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 // import dotenv from "dotenv";
-import ws from "ws";
+// import ws from "ws";
 
-neonConfig.webSocketConstructor = ws;
+// neonConfig.webSocketConstructor = ws;
 
 const connectionString = `${process.env.DATABASE_URL}`;
+
+
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
